acbaac83ed9288add03ba572e22c7e808373c7c2
Abner Oliveira <abner.oliveira.ce@gmail.com>
2020-03-30 20:12:16 -0300

    cadastro de usuario e login

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  secret: 'bad15578faa03492a04033174b0b0483',
+  expiresIn: '7d',
+};
---
+++ src/controllers/UserController.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcrypt');
+const Yup = require('yup');
+const connection = require('../database/connection');
+
+module.exports = {
+
+  async index(req, res) {
+    const users = await connection('users').select('*');
+    return res.json(users);
+  },
+
+  async create(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
+
+    const { name, email, password } = req.body;
+
+    // verificando o email já existe na base de dados
+    const userExists = await connection('users').where('email', email).select('*').first();
+    // se existir da erro 400
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já existe.' });
+    }
+
+    // encriptando a senha
+    const password_hash = await bcrypt.hash(password, 8);
+
+    await connection('users').insert({
+      name,
+      email,
+      password_hash,
+    });
+
+    return res.json({ name, email, password_hash });
+  },
+};
